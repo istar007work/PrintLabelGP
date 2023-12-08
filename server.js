f5348fa4ba88a11e02f7b2bce3f3bcc20637b956
ali <ali@geometris.com>
2023-12-08 12:54:04 -0600

    adding navbar

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'assets','images')));
+app.use(express.static(path.join(__dirname, 'assets','videos')));
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'index')));
+
+
+
+
+
+
+
+
+
+
+
+
+// Handle all other routes by serving the 'index.html' file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index', 'homepage.html'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
