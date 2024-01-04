d6d091db33fe0a6c0d2cf0e620405e147e7b53a5
ali <ali@geometris.com>
2024-01-04 17:47:15 -0600

    productpage

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const express = require('express');
 const path = require('path');
 
@@ -9,11 +10,14 @@ const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'assets','images')));
+app.use(express.static(path.join(__dirname, 'assets','products')));
 app.use(express.static(path.join(__dirname, 'assets','videos')));
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'index')));
 app.use(express.static(path.join(__dirname, 'javascript','homepage')));
 
+//
+app.use(express.json());
 
 
 
@@ -21,14 +25,20 @@ app.use(express.static(path.join(__dirname, 'javascript','homepage')));
 
 
 
-
-
-
+// All routes
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
 
 // Handle all other routes by serving the 'index.html' file
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index', 'homepage.html'));
+});
+
+// all_products.html page
+app.get("/products", (req, res) => {
+  res.sendFile(path.join(__dirname, 'index', 'all_products.html'));
 });
 
 
@@ -42,10 +52,10 @@ app.get('*', (req, res) => {
 
 
 
-
-
-
-// Start the server
+// start the server
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
